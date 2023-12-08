5915dd23780f6b2b5c57037d9ab2fa0f9b96e68b
Mick Jari <jari.zilverentant@gmail.com>
2023-12-08 20:59:27 +0100

    Initial Commit

---
+++ MiniMineSweeper Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiniMineSweeper Watch App
+//
+//  Created by Jari Zilverentant on 08/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniMineSweeper Watch App/MiniMineSweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMineSweeperApp.swift
+//  MiniMineSweeper Watch App
+//
+//  Created by Jari Zilverentant on 08/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniMineSweeper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
